772ff57e8e202f454c5c6db29390ac27c7d55eda
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2024-10-04 21:04:07 +0530

    Add initial project files and dependencies

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.KEY || "da708d9f09mshf1e10b3d24948b1p1d0717jsn8e08350550e1",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
