d70e1a01f68b4323dca75f515c81b90afce33151
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2024-10-05 12:43:56 +0530

    Refactor image alt attributes in SearchResult and VideoDetails components

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":
-            process.env.KEY || "da708d9f09mshf1e10b3d24948b1p1d0717jsn8e08350550e1",
+        "X-RapidAPI-Key": "da708d9f09mshf1e10b3d24948b1p1d0717jsn8e08350550e1",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
